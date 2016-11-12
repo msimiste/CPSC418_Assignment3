d35bdf0dfbc6f7d2f9ff81bb9a2290edeaf50d29
Mike Simister <mikesimister@shaw.ca>
2016-11-11 20:21:41 -0700

    initial commit, SG prime generation and prim roots tested and appear to be functional

---
+++ A3/src/PrimeUtil.java
@@ -0,0 +1,47 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+public class PrimeUtil {
+	
+	private static BigInteger two = BigInteger.ONE.add(BigInteger.ONE);
+	private static BigInteger one = BigInteger.ONE;
+
+	public PrimeUtil(){
+		
+	}
+	
+	public static BigInteger sopieGermainPrime(){
+		
+		Random rand = new Random(System.currentTimeMillis());
+		BigInteger sg = BigInteger.probablePrime(1023,rand);		
+		BigInteger sqTimes2 = sg.multiply(two);
+		BigInteger sqTimes2Add1 = sqTimes2.add(one);
+		
+		
+		while(sqTimes2Add1.isProbablePrime(3) == false){
+			sg = BigInteger.probablePrime(1023,rand);
+			two = BigInteger.ONE.add(one);
+			sqTimes2 = sg.multiply(two);
+			sqTimes2Add1 = sqTimes2.add(one);			
+		}
+		return sg;
+	}
+	
+	public static BigInteger primitiveRoot(BigInteger sophie){
+		
+		BigInteger pMinus1 = sophie.subtract(one);
+		BigInteger q = pMinus1.divide(two);
+		BigInteger smallExp = pMinus1.divide(q);		
+		boolean isPrimRoot = false;
+		BigInteger g = one;
+		while(!(isPrimRoot)){
+			g = g.add(one);
+			BigInteger smallTest = g.modPow(smallExp, sophie);
+			isPrimRoot = smallTest.compareTo(one) != 0;
+			BigInteger bigTest = g.modPow(q, sophie);
+			isPrimRoot = isPrimRoot && (bigTest.compareTo(one) != 0);			
+		}		
+		System.out.println(g);
+		return g;		
+	}
+}
